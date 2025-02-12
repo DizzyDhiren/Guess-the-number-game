ea7fbb50d32e293d9139787cf8c09483470a4bd6
DizzyDhiren <dhiren007patel@gmail.com>
2025-02-12 14:19:03 +0000

    Lower and Higher Hints work

--- myscript.js
+++ myscript.js
@@ -1,22 +1,38 @@
 const submitBtn = document.getElementById("submit");
-const hint = document.getElementById("hint");
+ 
 
+document.addEventListener("DOMContentLoaded", function() {
+    let hint = document.getElementById("hint");
+   
+});
 
 
 
 
 //generate random num between 1 & 100
 const randomNumber = Math.floor(Math.random()* 100)
-console.log(randomNumber);
+console.log(`random = `, randomNumber);
 
 //submitting user guess
 function submitGuess(){
     let userGuess = document.getElementById("user-guess");
     let userGuessValue = Number(userGuess.value);
-    console.log(userGuessValue);
+    console.log(`User Guess =`, userGuessValue);
     if (userGuessValue === randomNumber){
-        console.log("you win");
+        youWin(); 
+    } else if (userGuessValue < randomNumber){
+        hint.innerHTML = "Try a higher number!";
+    }else{
+        hint.innerHTML = "Try a lower number!"
     }
+
+}
+
+function youWin(){
+    let hiddenNumber = document.getElementById("random-number");
+    console.log(hiddenNumber);
+    hiddenNumber.innerHTML = `${randomNumber}`
+    hint.innerHTML = "YOU WIN";
 
 }
 
