87deea62257f7ccf6e803a0bbb8148020d25e7f2
DizzyDhiren <dhiren007patel@gmail.com>
2025-02-12 01:41:48 +0000

    function works and can see if user guess is === to random number// Need to add the hints

--- myscript.js
+++ myscript.js
@@ -1,2 +1,24 @@
+const submitBtn = document.getElementById("submit");
+const hint = document.getElementById("hint");
+
+
+
+
+
+//generate random num between 1 & 100
 const randomNumber = Math.floor(Math.random()* 100)
 console.log(randomNumber);
+
+//submitting user guess
+function submitGuess(){
+    let userGuess = document.getElementById("user-guess");
+    let userGuessValue = Number(userGuess.value);
+    console.log(userGuessValue);
+    if (userGuessValue === randomNumber){
+        console.log("you win");
+    }
+
+}
+
+
+
